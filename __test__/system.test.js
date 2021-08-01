c39a9642652f802f0907c4c464fb6b4db2656829
itsjustaplant <alperensahin442@gmail.com>
2021-08-01 20:17:19 +0300

    test: add test for system endpoint

---
+++ __test__/system.test.js
@@ -0,0 +1,18 @@
+/* eslint-disable require-jsdoc, no-unused-vars, max-len*/
+
+const Client = require('./../lib/client');
+const System = require('./../lib/system/system');
+
+const API_KEY = '13721ff14436d11b3165c206ce55d1d9';
+
+describe('System endpoint', () => {
+  const client = new Client(API_KEY);
+  const planName = 'FREE';
+  it('should get plan without error', async () => {
+    const result = await System.getSystem(client, planName);
+    const response = await result.json();
+    const {content} = response;
+    const {name} = content;
+    expect(name).toBe(planName);
+  });
+});
