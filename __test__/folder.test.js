5225a9298f4c2214cc7f8a175ffcb8cbc5f409d7
itsjustaplant <alperensahin442@gmail.com>
2021-08-01 20:35:40 +0300

    test: add test for folder endpoint and fix typo

---
+++ __test__/folder.test.js
@@ -0,0 +1,17 @@
+/* eslint-disable require-jsdoc, no-unused-vars, max-len*/
+
+const Client = require('./../lib/client');
+const Folder = require('./../lib/folder/folder');
+
+const API_KEY = '13721ff14436d11b3165c206ce55d1d9';
+
+describe('Folder endpoint', () => {
+  const client = new Client(API_KEY);
+  it('should gen folder without error', async () => {
+    const result = await Folder.getFolder(client, '60e84f985a364a3729660ca2');
+    const response = await result.json();
+    const {content} = response;
+    const {owner} = content;
+    expect(owner).toBe('justaplant');
+  });
+});
