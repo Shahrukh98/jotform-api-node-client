0d5e152ff07594d4ef5a5a288a83bb669c3d477d
itsjustaplant <alperensahin442@gmail.com>
2021-08-02 14:40:46 +0300

    test: add test for client

---
+++ __test__/client.test.js
@@ -0,0 +1,16 @@
+/* eslint-disable require-jsdoc, no-unused-vars, max-len*/
+
+const Client = require('./../lib/client');
+
+describe('Client test', () => {
+  const API_KEY = '13721ff14436d11b3165c206ce55d1d9';
+  it('should initialize client without error', () => {
+    const client = new Client(API_KEY);
+    expect(client.key).toBe(API_KEY);
+  });
+  it('should throw error when key is not defined', () => {
+    expect(() => {
+      const client = new Client();
+    }).toThrow('You need an api key to use client');
+  });
+});
