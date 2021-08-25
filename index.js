fdd35abfa6d98214a55fe3f5d3143354c157021a
itsjustaplant <alperensahin442@gmail.com>
2021-08-25 10:03:36 +0300

    chore: remove start script and index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable require-jsdoc, no-unused-vars, max-len*/
-
-const Client = require('./lib/client');
-const System = require('./lib/system/system');
-const Form = require('./lib/form/form');
-
-const API_KEY = '13721ff14436d11b3165c206ce55d1d9';
-const client = new Client(API_KEY);
-
